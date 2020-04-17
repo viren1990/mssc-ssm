14d57f5173bac3555aed2065edb7e9cc7d6db6e7
Viren <viren.sharma.1990@gmail.com>
2020-04-17 14:34:55 +0530

    add .circleci artifacts

--- src/test/java/io/viren/msscssm/services/PaymentServiceIT.java
+++ src/test/java/io/viren/msscssm/services/PaymentServiceIT.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringJUnitConfig
 @SpringBootTest
-public class PaymentServiceImplTest {
+public class PaymentServiceIT {
 
     @Autowired
     private PaymentService paymentService;
